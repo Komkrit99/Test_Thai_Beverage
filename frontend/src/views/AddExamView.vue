717519a36be2604593d3dd15bb2093f70f05bf86
Komkrit99 <nate.kt1999@gmail.com>
2025-06-16 23:47:54 +0700

    test log

--- frontend/src/views/AddExamView.vue
+++ frontend/src/views/AddExamView.vue
@@ -117,7 +117,7 @@ const form = ref({
   option4: '',
   correctAnswer: ''
 })
-console.log(process.env.VITE_API_URL)
+console.log(import.meta.env)
 const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080'
 
 const saveExam = async () => {
